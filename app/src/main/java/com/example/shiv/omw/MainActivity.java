d580a5163a0c670e7e2b4cf0e656c4d9bbd6a09b
christopherni <cni5866@gmail.com>
2017-02-18 04:15:24 -0800

    Added google place autocomplete.

--- app/src/main/java/com/example/shiv/omw/MainActivity.java
+++ app/src/main/java/com/example/shiv/omw/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.shiv.omw;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -10,27 +11,33 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.text.DateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener, PlaceSelectionListener {
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0 ;
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
     private LocationRequest mLocationRequest;
     private String mLastUpdateTime;
+    private String mPlaceName;
+    private LatLng mLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,21 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                     .build();
         }
         setContentView(R.layout.activity_main);
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(this);/*new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+            }
+        });*/
     }
 
     @Override
@@ -76,6 +98,13 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         }
     }
 
+    public void mapScreen(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra("LAT", mLatLng.latitude);
+        intent.putExtra("LONG", mLatLng.longitude);
+        startActivity(intent);
+    }
+
     @Override
     public void onConnected(Bundle connectionHint) {
         if (ContextCompat.checkSelfPermission(this,
@@ -89,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
             startLocationUpdates();
         } catch (SecurityException e) {
             Toast.makeText(getApplicationContext(),
-                    "Location failed, please try again.", Toast.LENGTH_LONG).show();
+                    "Location failed.", Toast.LENGTH_LONG).show();
         }
 
     }
@@ -125,10 +154,16 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        updateUI();
     }
 
-    private void updateUI() {
-        ((TextView) findViewById(R.id.textview)).setText(String.valueOf(mCurrentLocation.getLatitude()) + mCurrentLocation.getLongitude());
+    @Override
+    public void onPlaceSelected(Place place) {
+        mPlaceName = place.getName().toString();
+        mLatLng= place.getLatLng();
+    }
+
+    @Override
+    public void onError(Status status) {
+
     }
 }
